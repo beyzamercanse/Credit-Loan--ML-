801b9287622f5ee2fbc8208f1d5f9a24896c3e0b
Beyza Mercan <beyzamercanse@gmail.com>
2023-10-01 21:47:31 +0300

    update final

---
+++ flask_file.py
@@ -0,0 +1,7 @@
+# Import necessary libraries
+import pandas as pd  # For data manipulation and analysis
+import seaborn as sns  # For statistical data visualization
+# For splitting data into training and testing sets
+from sklearn.model_selection import train_test_split
+# For building web applications and handling HTTP requests
+from flask import Flask, request
